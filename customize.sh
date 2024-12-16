08ad74d0e4001affc24a6cdd05aec6ad90b6e2e8
Spelly <michicaputo6@gmail.com>
2024-12-16 17:04:52 +0100

    1.5 overhaul

--- customize.sh
+++ customize.sh
@@ -1,9 +1,28 @@
 #!/system/bin/sh
 echo "Running customize.sh" >> /data/local/tmp/magisk_module.log
 
-# Thanking the user. thank you btw
-echo "Welcome to Treblefy by Spelly! Grab a glass of water and relax while i do the magic!"
+# bomb splash
+echo "
+ ___________  _______    _______  _______   ___       _______   _______  ___  ___   
+("     _   ")/"      \  /"     "||   _  "\ |"  |     /"     "| /"     "||"  \/"  |  
+ )__/  \\__/|:        |(: ______)(. |_)  :)||  |    (: ______)(: ______) \   \  /   
+    \\_ /   |_____/   ) \/    |  |:     \/ |:  |     \/    |   \/    |    \\  \/    
+    |.  |    //      /  // ___)_ (|  _  \\  \  |___  // ___)_  // ___)    /   /     
+    \:  |   |:  __   \ (:      "||: |_)  :)( \_|:  \(:      "|(:  (      /   /      
+     \__|   |__|  \___) \_______)(_______/  \_______)\_______) \__/     |___/       
+                                                                                    
+ _______  ___  ___       ________  _______    _______  ___      ___       ___  ___  
+|   _  "\|"  \/"  |     /"       )|   __ "\  /"     "||"  |    |"  |     |"  \/"  | 
+(. |_)  :)\   \  /     (:   \___/ (. |__) :)(: ______)||  |    ||  |      \   \  /  
+|:     \/  \\  \/       \___  \   |:  ____/  \/    |  |:  |    |:  |       \\  \/   
+(|  _  \\  /   /         __/  \\  (|  /      // ___)_  \  |___  \  |___    /   /    
+|: |_)  :)/   /         /" \   :)/|__/ \    (:      "|( \_|:  \( \_|:  \  /   /     
+(_______/|___/         (_______/(_______)    \_______) \_______)\_______)|___/      
+                                                                                    
+"
 
+# Notifying user that installation has been started
+echo "Installation started..."
 # Set permissions for system.prop
 MODPATH=$NVBASE/modules/treblefy
 set_perm $MODPATH/system.prop 0 0 0644
@@ -12,4 +31,4 @@ set_perm $MODPATH/system.prop 0 0 0644
 su -c "cmd package bg-dexopt-job"
 
 # Aaaand... it's done.
-echo "Module installation finished, i hope you will have a great experience with it!"
+echo "Module installation finished, i hope you will have a great experience with it! Reboot to apply all changes!"
